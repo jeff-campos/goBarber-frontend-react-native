69882958b894e0b3265a0f33337620b2c136ccef
Admin <admin@Jeff-Campos-MBP.lan>
2020-03-02 06:46:56 -0300

    Feat(initial): created code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Teste from '~/teste';
+
+const App: () => React$Node = () => {
+  return <Teste />;
+};
+
+export default App;
---
+++ src/teste.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Teste: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+};
+
+export default Teste;
