d1e5286e84a712e2f1f8aa7d5d1384eda4904da6
Admin <admin@Jeff-Campos-MBP.lan>
2020-03-03 07:11:01 -0300

    Feat(initial): pages and routes

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+
+export default function Routes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#7159c1' },
+        headerTintColor: '#fff',
+      }}>
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ title: 'Login' }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{ title: 'Cadastre-se' }}
+      />
+    </Stack.Navigator>
+  );
+}
